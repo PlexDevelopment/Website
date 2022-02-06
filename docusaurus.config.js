8f7f3c5f12d0f870bab64862558e26cea191b486
Telesphoreo <me@telesphoreo.me>
2022-02-05 19:12:22 -0600

    Add Javadocs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,12 +17,12 @@ const config = {
   projectName: 'docs', // Usually your repo name.
 
   presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+  [
+  'classic',
+  /** @type {import('@docusaurus/preset-classic').Options} */
+  ({
+    docs: {
+      sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/PlexDevelopment/docs/tree/master',
         },
@@ -30,88 +30,97 @@ const config = {
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
-    ],
+  ],
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Plex',
-        logo: {
-          alt: 'Plex Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Introduction',
-          },
-	  {
-	    type: 'doc',
-	    docId: 'compiling',
-	    position: 'left',
-	    label: 'Compiling',
-	  },
-          {
-            position: 'right',
-            label: 'GitHub',
-	    href: 'https://github.com/PlexDevelopment',
-          },
-        ],
+  /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+  ({
+    navbar: {
+      title: 'Plex',
+      logo: {
+        alt: 'Plex Logo',
+        src: 'img/logo.svg',
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Introduction',
-                to: '/docs/intro',
-              },
-	      {
-		label: 'Compiling',
-		to: '/docs/compiling',
-	      }
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Forums',
-                href: 'https://forum.plex.us.org',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/HZsdUnsRKc',
-              },
-            ],
-          },
-          {
-            title: 'GitHub',
-            items: [
-              {
-                label: 'Plex',
-                href: 'https://github.com/PlexDevelopment/Plex',
-              },
-	      {
-		label: 'Docs',
-		href: 'https://github.com/PlexDevelopment/docs',
-	      }
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Plex. Symbols from <a href="https://fontawesome.com/license">FontAwesome</a>. Built with Docusaurus.`,
+      items: [
+      {
+        type: 'doc',
+        docId: 'intro',
+        position: 'left',
+        label: 'Introduction',
       },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
+      {
+       type: 'doc',
+       docId: 'compiling',
+       position: 'left',
+       label: 'Compiling',
+     },
+     {
+       position: 'left',
+       label: 'Javadocs',
+       to: 'pathname:///javadocs',
+     },
+     {
+      position: 'right',
+      label: 'GitHub',
+      href: 'https://github.com/PlexDevelopment',
+    },
+    ],
+  },
+  footer: {
+    style: 'dark',
+    links: [
+    {
+      title: 'Docs',
+      items: [
+      {
+        label: 'Introduction',
+        to: '/docs/intro',
       },
-    }),
+      {
+        label: 'Compiling',
+        to: '/docs/compiling',
+      },
+      {
+        label: 'Javadocs',
+        to: 'pathname:///javadocs',
+      },
+      ],
+    },
+    {
+      title: 'Community',
+      items: [
+      {
+        label: 'Forums',
+        href: 'https://forum.plex.us.org',
+      },
+      {
+        label: 'Discord',
+        href: 'https://discord.gg/HZsdUnsRKc',
+      },
+      ],
+    },
+    {
+      title: 'GitHub',
+      items: [
+      {
+        label: 'Plex',
+        href: 'https://github.com/PlexDevelopment/Plex',
+      },
+      {
+        label: 'Docs',
+        href: 'https://github.com/PlexDevelopment/docs',
+      }
+      ],
+    },
+    ],
+    copyright: `Copyright © ${new Date().getFullYear()} Plex. Symbols from <a href="https://fontawesome.com/license">FontAwesome</a>. Built with Docusaurus.`,
+  },
+  prism: {
+    theme: lightCodeTheme,
+    darkTheme: darkCodeTheme,
+  },
+}),
 };
 
 module.exports = config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Home`}
+      description="The official documentation for Plex.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
